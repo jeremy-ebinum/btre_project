58f78bce7a5360e38c0d99bb31d9ecd946bcb3a9
jeremy-ebinum <jeremy.ebinum@gmail.com>
2019-10-24 18:42:35 +0100

    Listings page functional

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,28 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from .models import Listing
+
 
 def index(request):
-    return render(request, "listings/listings.html")
+    listing_list = (
+        Listing.objects.select_related("realtor")
+        .all()
+        .order_by("-list_date")
+        .filter(is_published=True)
+    )
+    paginator = Paginator(listing_list, 6)
+    page = request.GET.get("page")
+    pages = paginator.page_range
+    listings = paginator.get_page(page)
+
+    context = {"listings": listings, "pages": pages}
+
+    return render(request, "listings/listings.html", context)
+
 
 def listing(request, listing_id):
     return render(request, "listings/listing.html")
+
 
 def search(request):
     return render(request, "listings/search.html")
