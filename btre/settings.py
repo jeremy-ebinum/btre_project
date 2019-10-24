aeded3577273c9d0461bb3dcbd1a9d6a291cfef1
jeremy-ebinum <jeremy.ebinum@gmail.com>
2019-10-24 10:41:14 +0100

    Created and migrated listings and realtors models.

--- btre/settings.py
+++ btre/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = "btre.wsgi.application"
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': '0000',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
