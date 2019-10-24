577ef13441dc08de3a552e108b9730c9ef040b84
jeremy-ebinum <jeremy.ebinum@gmail.com>
2019-10-24 19:50:24 +0100

    index and about pages functional.

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,30 @@
+from django.db.models import Count
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Listing
+from realtors.models import Realtor
 
 
 def index(request):
-    return render(request, "pages/index.html")
+    listings = (
+        Listing.objects.select_related("realtor")
+        .order_by("-list_date")
+        .filter(is_published=True)[:3]
+    )
+    context = {"listings": listings}
+    return render(request, "pages/index.html", context)
 
 
 def about(request):
-    return render(request, "pages/about.html")
+    # Get all realtors
+    realtors = (
+        Realtor.objects.annotate(num_listings=Count("listing"))
+        .order_by("-num_listings")
+        .all()
+    )
+
+    # Get MVP
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {"realtors": realtors, "mvp_realtors": mvp_realtors}
+    return render(request, "pages/about.html", context)
