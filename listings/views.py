f889eb0e8094700b20100c957dbe09ffcf67dc51
jeremy-ebinum <jeremy.ebinum@gmail.com>
2019-10-25 11:44:18 +0100

    Search forms and pages functional.

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from .models import Listing
+from .choices import bedroom_choices, price_choices, state_choices
 
 
 def index(request):
@@ -12,10 +13,9 @@ def index(request):
     )
     paginator = Paginator(listing_list, 6)
     page = request.GET.get("page")
-    pages = paginator.page_range
     listings = paginator.get_page(page)
 
-    context = {"listings": listings, "pages": pages}
+    context = {"listings": listings}
 
     return render(request, "listings/listings.html", context)
 
@@ -40,4 +40,48 @@ def listing(request, listing_id):
 
 
 def search(request):
-    return render(request, "listings/search.html")
+    queryset_list = Listing.objects.select_related("realtor").order_by("-list_date")
+
+    # Keywords
+    if "keywords" in request.GET:
+        keywords = request.GET["keywords"]
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+
+    # City
+    if "city" in request.GET:
+        city = request.GET["city"]
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
+
+    # State
+    if "state" in request.GET:
+        state = request.GET["state"]
+        if state:
+            queryset_list = queryset_list.filter(state__iexact=state)
+
+    # Bedrooms
+    if "bedrooms" in request.GET:
+        bedrooms = request.GET["bedrooms"]
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms).order_by(
+                "-bedrooms"
+            )
+
+    # Price
+    if "price" in request.GET:
+        price = request.GET["price"]
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price).order_by("-price")
+
+    paginator = Paginator(queryset_list, 12)
+    page = request.GET.get("page")
+    listings = paginator.get_page(page)
+    context = {
+        "state_choices": state_choices,
+        "bedroom_choices": bedroom_choices,
+        "price_choices": price_choices,
+        "listings": listings,
+        "values": request.GET,
+    }
+    return render(request, "listings/search.html", context)
